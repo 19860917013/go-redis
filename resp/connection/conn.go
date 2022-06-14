595fea04d477ebb9b39d7e0934c87651ec0d49bb
19860917013 <913552312@qq.com>
2022-06-14 12:40:24 +0800

    协议层方法 tidy

--- resp/connection/conn.go
+++ resp/connection/conn.go
@@ -1,7 +1,6 @@
 package connection
 
 import (
-	"bytes"
 	"go-redis/lib/sync/wait"
 	"net"
 	"sync"
@@ -9,6 +8,7 @@ import (
 )
 
 // Connection redis-cli 协议层对连接的描述
+// 扩展一下的话连接 Connection 可以加密码等字段
 type Connection struct {
 	conn net.Conn
 
@@ -17,17 +17,6 @@ type Connection struct {
 
 	// 操作一个客户上一把锁 并发问题
 	mu sync.Mutex
-
-	// subscribing channels
-	subs map[string]bool
-
-	// password may be changed by CONFIG command during runtime, so store the password
-	password string
-
-	// queued commands for `multi`
-	multiState bool
-	queue      [][][]byte
-	watching   map[string]uint32
 
 	// selected db
 	selectedDB int
@@ -69,94 +58,6 @@ func (c *Connection) Write(b []byte) error {
 	return err
 }
 
-// Subscribe add current connection into subscribers of the given channel
-func (c *Connection) Subscribe(channel string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if c.subs == nil {
-		c.subs = make(map[string]bool)
-	}
-	c.subs[channel] = true
-}
-
-// UnSubscribe removes current connection into subscribers of the given channel
-func (c *Connection) UnSubscribe(channel string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if len(c.subs) == 0 {
-		return
-	}
-	delete(c.subs, channel)
-}
-
-// SubsCount returns the number of subscribing channels
-func (c *Connection) SubsCount() int {
-	return len(c.subs)
-}
-
-// GetChannels returns all subscribing channels
-func (c *Connection) GetChannels() []string {
-	if c.subs == nil {
-		return make([]string, 0)
-	}
-	channels := make([]string, len(c.subs))
-	i := 0
-	for channel := range c.subs {
-		channels[i] = channel
-		i++
-	}
-	return channels
-}
-
-// SetPassword stores password for authentication
-func (c *Connection) SetPassword(password string) {
-	c.password = password
-}
-
-// GetPassword get password for authentication
-func (c *Connection) GetPassword() string {
-	return c.password
-}
-
-// InMultiState tells is connection in an uncommitted transaction
-func (c *Connection) InMultiState() bool {
-	return c.multiState
-}
-
-// SetMultiState sets transaction flag
-func (c *Connection) SetMultiState(state bool) {
-	if !state { // reset data when cancel multi
-		c.watching = nil
-		c.queue = nil
-	}
-	c.multiState = state
-}
-
-// GetQueuedCmdLine returns queued commands of current transaction
-func (c *Connection) GetQueuedCmdLine() [][][]byte {
-	return c.queue
-}
-
-// EnqueueCmd  enqueues command of current transaction
-func (c *Connection) EnqueueCmd(cmdLine [][]byte) {
-	c.queue = append(c.queue, cmdLine)
-}
-
-// ClearQueuedCmds clears queued commands of current transaction
-func (c *Connection) ClearQueuedCmds() {
-	c.queue = nil
-}
-
-// GetWatching returns watching keys and their version code when started watching
-func (c *Connection) GetWatching() map[string]uint32 {
-	if c.watching == nil {
-		c.watching = make(map[string]uint32)
-	}
-	return c.watching
-}
-
 // GetDBIndex returns selected db
 func (c *Connection) GetDBIndex() int {
 	return c.selectedDB
@@ -165,26 +66,4 @@ func (c *Connection) GetDBIndex() int {
 // SelectDB selects a database
 func (c *Connection) SelectDB(dbNum int) {
 	c.selectedDB = dbNum
-}
-
-// FakeConn implements redis.Connection for test
-type FakeConn struct {
-	Connection
-	buf bytes.Buffer
-}
-
-// Write writes data to buffer
-func (c *FakeConn) Write(b []byte) error {
-	c.buf.Write(b)
-	return nil
-}
-
-// Clean resets the buffer
-func (c *FakeConn) Clean() {
-	c.buf.Reset()
-}
-
-// Bytes returns written data
-func (c *FakeConn) Bytes() []byte {
-	return c.buf.Bytes()
 }
